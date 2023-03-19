7ebf86e8eb8547a34b200846ba4f439cefd1c0e4
Shimokazu96 <aloe1dental@gmail.com>
2023-03-20 08:47:26 +0900

    グーグルアナリティクスを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,55 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+export const pageview = (url: any) => {
+  if (!GA_MEASUREMENT_ID) return;
+  window.gtag("config", GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
