b40aed3079c3756719859778e7dacefd05b5ca79
Shimokazu96 <aloe1dental@gmail.com>
2025-06-22 14:44:59 +0900

    画像ドメインリストを更新し、Instagram関連のドメインを追加

--- next.config.js
+++ next.config.js
@@ -5,8 +5,13 @@ const nextConfig = {
     images: {
     domains: [
       'scontent.cdninstagram.com',
+      'scontent-nrt1-1.cdninstagram.com',
+      'scontent-nrt2-2.cdninstagram.com',
+      'scontent-iad3-1.cdninstagram.com',
+      'scontent-nrt1-1.cdninstagram.com',
+      'scontent-lax3-2.cdninstagram.com',
       'scontent.xx.fbcdn.net',
-      'instagram.com'
+      'instagram.com',
     ],
   },
 }
